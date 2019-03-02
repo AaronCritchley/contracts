fcebd4351fa82b9d560c937411c1c4c41f74c266
Dave Willmer <dave.willmer@gmail.com>
2019-03-02 12:54:32 +0000

    Remove nosetests and replace with pytest. Add Travis CI

--- tests/test_meta.py
+++ tests/test_meta.py
@@ -1,169 +1,175 @@
+
 from abc import abstractmethod
-import functools
-import nose
-import unittest
+
+import pytest
+from pytest import raises
 
 from contracts import ContractNotRespected, contract, ContractsMeta
 from contracts import CannotDecorateClassmethods
 from contracts import with_metaclass
 
-def expected_failure(test):
-    @functools.wraps(test)
-    def inner(*args, **kwargs):
-        try:
-            test(*args, **kwargs)
-        except Exception:
-            raise nose.SkipTest
-        else:
-            raise AssertionError('Failure expected')
-    return inner
 
+def test_meta_still_works1():
 
-class TestMeta(unittest.TestCase):
+    class A(with_metaclass(ContractsMeta, object)):
 
-    def test_meta_still_works1(self):
-
-        class A(with_metaclass(ContractsMeta, object)):
-
-            @abstractmethod
-            @contract(a='>0')
-            def f(self, a):
-                pass
-
-        class B(A):
-            # does not implement f
+        @abstractmethod
+        @contract(a='>0')
+        def f(self, a):
             pass
 
-        self.assertRaises(TypeError, B)
+    class B(A):
+        # does not implement f
+        pass
 
-    def test_meta_still_works2(self):
+    with raises(TypeError):
+        B()
 
-        class A(with_metaclass(ContractsMeta, object)):
 
-            # inverse order
-            @contract(a='>0')
-            @abstractmethod
-            def f(self, a):
-                pass
+def test_meta_still_works2():
 
-        class B(A):
-            # does not implement f
+    class A(with_metaclass(ContractsMeta, object)):
+
+        # inverse order
+        @contract(a='>0')
+        @abstractmethod
+        def f(self, a):
+            pass
+
+    class B(A):
+        # does not implement f
+        pass
+
+    with raises(TypeError):
+        B()
+
+
+def test_meta1():
+
+    class A(with_metaclass(ContractsMeta, object)):
+
+        @abstractmethod
+        @contract(a='>0')
+        def f(self, a):
             pass
 
 
-        self.assertRaises(TypeError, B)
+        @contract(a='>0')
+        @abstractmethod
+        def g(self, a):
+            pass
 
-    def test_meta1(self):
+    class B(A):
+
+        def f(self, a):
+            pass
+
+        def g(self, a):
+            pass
+
+    b = B()
+
+    with raises(ContractNotRespected):
+        b.f(0)
+
+    with raises(ContractNotRespected):
+        b.g(0)
+
+
+@pytest.mark.xfail
+def test_static1():
+
+    class A(with_metaclass(ContractsMeta, object)):
+
+        @staticmethod
+        @contract(a='>0')
+        def f(a):
+            pass
+
+    with raises(ContractNotRespected):
+        A.f(0)
+
+    class B(A):
+
+        @staticmethod
+        def f(a):
+            pass
+
+    with raises(ContractNotRespected):
+        B.f(0) # this doesn't work
+
+
+@pytest.mark.xfail
+def test_classmethod1():
+
+    class A(with_metaclass(ContractsMeta, object)):
+
+        @classmethod
+        @contract(a='>0')
+        def f(cls, a):
+            print('called A.f(%s)' % a)
+            pass
+
+    with raises(ContractNotRespected):
+        A.f(0)
+
+    class B(A):
+
+        @classmethod
+        def f(cls, a):
+            print('called B.f(%s)' % a)
+            pass
+
+    with raises(ContractNotRespected):
+        B.f(0) # this doesn't work
+
+
+@pytest.mark.xfail
+def test_classmethod1ns():
+
+    class A(with_metaclass(ContractsMeta, object)):
+
+        @classmethod
+        @contract(a='>0')
+        def f(cls, a):
+            print('called A.f(%s)' % a)
+            pass
+
+    with raises(ContractNotRespected):
+        A.f(0)
+
+    class B(A):
+
+        @classmethod
+        def f(cls, a):
+            print('called B.f(%s)' % a)
+            pass
+
+    with raises(ContractNotRespected):
+        B.f(0) # this doesn't work
+
+
+def test_classmethod2a():
+
+    def test_classmethod2():
 
         class A(with_metaclass(ContractsMeta, object)):
 
-            @abstractmethod
             @contract(a='>0')
-            def f(self, a):
-                pass
-
-
-            @contract(a='>0')
-            @abstractmethod
-            def g(self, a):
+            @classmethod
+            def f(cls, a):
                 pass
 
         class B(A):
 
-            def f(self, a):
-                pass
-
-            def g(self, a):
-                pass
-
-        b = B()
-
-        self.assertRaises(ContractNotRespected, b.f, 0)
-        self.assertRaises(ContractNotRespected, b.g, 0)
-
-    @expected_failure
-    def test_static1(self):
-
-        class A(with_metaclass(ContractsMeta, object)):
-
-            @staticmethod
-            @contract(a='>0')
-            def f(a):
-                pass
-
-        self.assertRaises(ContractNotRespected, A.f, 0)
-
-        class B(A):
-
-            @staticmethod
-            def f(a):
-                pass
-
-        self.assertRaises(ContractNotRespected, B.f, 0) # this doesn't work
-
-    @expected_failure
-    def test_classmethod1(self):
-
-        class A(with_metaclass(ContractsMeta, object)):
-
-            @classmethod
-            @contract(a='>0')
-            def f(cls, a):
-                print('called A.f(%s)' % a)
-                pass
-
-        self.assertRaises(ContractNotRespected, A.f, 0)
-
-        class B(A):
-
             @classmethod
             def f(cls, a):
-                print('called B.f(%s)' % a)
                 pass
 
-        self.assertRaises(ContractNotRespected, B.f, 0) # this doesn't work
-
-    @expected_failure
-    def test_classmethod1ns(self):
-
-        class A(with_metaclass(ContractsMeta, object)):
-
-            @classmethod
-            @contract(a='>0')
-            def f(cls, a):
-                print('called A.f(%s)' % a)
-                pass
-
-        self.assertRaises(ContractNotRespected, A.f, 0)
-
-        class B(A):
-
-            @classmethod
-            def f(cls, a):
-                print('called B.f(%s)' % a)
-                pass
-
-        self.assertRaises(ContractNotRespected, B.f, 0) # this doesn't work
+    with raises(CannotDecorateClassmethods):
+        test_classmethod2()
 
 
-    def test_classmethod2a(self):
-
-        def test_classmethod2():
-
-            class A(with_metaclass(ContractsMeta, object)):
-
-                @contract(a='>0')
-                @classmethod
-                def f(cls, a):
-                    pass
-
-            class B(A):
-
-                @classmethod
-                def f(cls, a):
-                    pass
-
-        self.assertRaises(CannotDecorateClassmethods, test_classmethod2)
-
-
-
+if __name__ == '__main__':
+    import pytest
+    pytest.main([__file__])
